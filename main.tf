df61bd76fedd48d1d1316176be4b0a7d7d1a3e6d
Callum <callumccr@gmail.com>
2020-08-23 17:18:46 +0100

    Gitlab README commit

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_appsync_resolver" "default" {
-  for_each          = var.enabled == true ? { for r in var.resolver_config : r.field => r } : {}
+  for_each          = var.enabled == true ? { for r in var.resolver_config : r.id => r } : {}
   type              = each.value.type
   api_id            = var.api_id
   data_source       = each.value.data_source
