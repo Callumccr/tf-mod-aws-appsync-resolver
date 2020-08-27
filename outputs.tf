877e4dcc8ba377f648505aa3d807d4759bdbdb07
Callum <callumccr@gmail.com>
2020-08-27 17:47:10 +0100

    Gitlab README commit

--- outputs.tf
+++ outputs.tf
@@ -2,19 +2,7 @@
 # Outputs: TF-MOD-AWS-APPSYNC-RESOLVER
 # -----------------------------------------------------------------------------
 
-output "resolver_config" {
-  value       = [{ for r in var.resolver_config : r.id => r... }, { for r in var.pipeline_resolver_config : r.field => r... }]
+output "config" {
+  value       = [merge({ for r in aws_appsync_resolver.default : r.id => { "id" = r.id, "arn" = r.arn } }, { for r in var.pipeline_resolver_config : r.field => { "id" = r.field, "arn" = r.arn } })]
   description = "A list of resolver ARNs"
 }
-
-output "resolver_arn" {
-  value       = [for r in aws_appsync_resolver.default : r.arn]
-  description = "A list of pipeline resolver ARNs"
-}
-
-output "pipeline_resolver_arn" {
-  value       = [for r in aws_appsync_resolver.pipeline : r.arn]
-  description = "A list of pipeline resolver ARNs"
-}
-
-
